2950d79f1505a8d62752afd1ac5aabb2929361e5
Steven-umg <svasquezc12@miumg.edu.gt>
2025-03-29 21:45:41 -0600

    Update celda.cpp

--- BuscaMinas/BuscaMinas/src/celda.cpp
+++ BuscaMinas/BuscaMinas/src/celda.cpp
@@ -1,57 +1,158 @@
-#include "Celda.h"
 #include <iostream>
+#include <unistd.h>
+#include "Config.h"
 using namespace std;
-Celda::Celda()
+
+// Constructor que inicializa los parámetros del tablero
+Config::Config(int filasTablero, int columnasTablero, int minasTablero, bool modoDesarrolladorTablero, int vidasTablero)
 {
+    this->filasTablero = filasTablero;                // Asigna el número de filas al tablero
+    this->columnasTablero = columnasTablero;          // Asigna el número de columnas al tablero
+    this->minasTablero = minasTablero;                // Asigna el número de minas al tablero
+    this->modoDesarrolladorTablero = modoDesarrolladorTablero; // Establece el modo de desarrollador (true o false)
+    this->vidasTablero = vidasTablero;                // Establece el número de vidas del jugador
 }
-Celda::Celda(int coordenadaX, int coordenadaY, bool estadoMina)
+
+// Función que muestra el menú de configuración y permite modificar valores
+void Config::menuConfiguracion()
 {
-    this->coordenadaX = coordenadaX;
-    this->coordenadaY = coordenadaY;
-    this->mina = estadoMina;
-    this->minaDescubierta = false;
-}
-int Celda::setCoordenadaX(int coordenadaX)
-{
-    this->coordenadaX = coordenadaX;
-}
-int Celda::getCoordenadaX()
-{
-    return this->coordenadaX;
-}
-int Celda::setCoordenadaY(int coordenadaY)
-{
-    this->coordenadaY = coordenadaY;
-}
-int Celda::getCoordenadaY()
-{
-    return this->coordenadaY;
-}
-bool Celda::setMina(bool estadoMina)
-{
-    if (this->getMina())
+    int opciones;            // Variable para almacenar la opción seleccionada
+    int valorIngresado;      // Variable para almacenar el valor que se desea cambiar
+    bool repetir = true;     // Variable para controlar el bucle del menú
+
+    do
     {
-        return false;
-    }
-    else{
-        this->mina = estadoMina;
-        return true;
-    }
+        system("cls"); // Limpiar la pantalla (función específica de Windows, en sistemas UNIX se puede usar "clear")
+
+        // Mostrar la configuración actual
+        cout << "\n\n\t\tCONFIGURACION ACTUAL -Menu-" << endl;
+        cout << "\t\t-------------------------------------------"<< endl;
+        cout << "\t\t1. Filas del Tablero ----> " << this->getfilasTablero() << endl;
+        cout << "\t\t2. Columnas del Tablero -> " << this->getcolumnasTablero() << endl;
+        cout << "\t\t3. Minas del Tablero ----> " << this->getminasTablero() << endl;
+        cout << "\t\t4. Modo del Juego -------> " << this->getmodoDesarrolladorTablero() << endl;
+        cout << "\t\t5. Vidas del Jugador ----> " << this->getvidasTablero() << endl;
+        cout << "\t\t6. Regresar al menu general" << endl;
+        cout << "\n\t\tIngrese una opcion: ";
+        cin >> opciones; // Solicitar al usuario que ingrese una opción
+
+        // Si la opción seleccionada no es 6 (salir), se solicita el nuevo valor
+        if (opciones != 6)
+        {
+            cout << "\n\tIngrese el valor que desea cambiar: ";
+            cin >> valorIngresado; // Solicitar al usuario el valor que quiere ingresar
+        }
+
+        // Procesar la opción seleccionada
+        switch (opciones)
+        {
+        case 1:
+            // Modificar el número de filas si el valor está entre 3 y 40
+            if (valorIngresado >= 3 && valorIngresado <= 40)
+            {
+                this->setfilasTablero(valorIngresado);  // Actualiza el número de filas
+                cout << "Filas del Tablero actualizadas" << endl;
+            }
+            else
+            {
+                // Si el valor está fuera de rango, mostrar un error
+                cout << "Error: El numero de Filas debe estar entre 3 y 40." << endl;
+            }
+            break;
+
+        case 2:
+            // Modificar el número de columnas
+            this->setcolumnasTablero(valorIngresado); 
+            cout << "Columnas del Tablero actualizadas" << endl;
+            break;
+
+        case 3:
+            // Modificar el número de minas
+            this->setminasTablero(valorIngresado); 
+            cout << "Minas del Tablero actualizadas" << endl;
+            break;
+
+        case 4:
+            // Modificar el modo de desarrollador
+            this->setmodoDesarrolladorTablero(valorIngresado); 
+            cout << "Modo del Juego actualizado" << endl;
+            break;
+
+        case 5:
+            // Modificar el número de vidas
+            this->setvidasTablero(valorIngresado); 
+            cout << "Vidas del Juego actualizadas" << endl;
+            break;
+
+        case 6:
+            // Salir del menú de configuración
+            repetir = false;
+            break;
+        }
+
+        system("pause"); // Pausar la ejecución para que el usuario vea los cambios
+    } while (repetir); // Continuar mostrando el menú hasta que el usuario seleccione la opción 6
 }
-bool Celda::getMina()
+
+// Métodos getter y setter para las variables de la clase
+
+// Devuelve el número de filas
+int Config::getfilasTablero()
 {
-    return this->mina;
+    return this->filasTablero;
 }
-bool Celda::setMinaDescubierta(bool minaDescubierta)
+
+// Establece el número de filas
+int Config::setfilasTablero(int filasTablero)
 {
-    this->minaDescubierta = minaDescubierta;
+    this->filasTablero = filasTablero;
 }
-bool Celda::getMinaDescubierta()
+
+// Devuelve el número de columnas
+int Config::getcolumnasTablero()
 {
-    return this->minaDescubierta;
+    return this->columnasTablero;
 }
-void Celda::imprimirCelda()
+
+// Establece el número de columnas
+int Config::setcolumnasTablero(int columnasTablero)
 {
-    cout << "Celda en " << this->coordenadaX << ", " << this->coordenadaY << " con mina? " << this->mina << "\n";
+    this->columnasTablero = columnasTablero;
+}
+
+// Devuelve el número de minas
+int Config::getminasTablero()
+{
+    return this->minasTablero;
+}
+
+// Establece el número de minas
+int Config::setminasTablero(int minasTablero)
+{
+    this->minasTablero = minasTablero;
+}
+
+// Devuelve el valor de modo desarrollador (true/false)
+bool Config::getmodoDesarrolladorTablero()
+{
+    return this->modoDesarrolladorTablero;
+}
+
+// Establece el valor de modo desarrollador (true/false)
+bool Config::setmodoDesarrolladorTablero(bool modoDesarrolladorTablero)
+{
+    this->modoDesarrolladorTablero = modoDesarrolladorTablero;
+}
+
+// Devuelve el número de vidas
+int Config::getvidasTablero()
+{
+    return this->vidasTablero;
+}
+
+// Establece el número de vidas
+int Config::setvidasTablero(int vidasTablero)
+{
+    this->vidasTablero = vidasTablero;
 }
 
